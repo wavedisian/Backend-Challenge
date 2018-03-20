295207e7656e26aae057076de8bf7f854d83b54e
BT_1N3_03 <BT_1N3_03@BT_1N3_03.bktech.local>
2018-03-20 09:22:46 -0400

    asdasdas

---
+++ Backend-Challenge/src/src/Book.java
@@ -0,0 +1,6 @@
+package src;
+
+public class Book {
+	private String 
+
+}
---
+++ Backend-Challenge/src/src/CSVReader.java
@@ -0,0 +1,59 @@
+package src;
+
+import java.awt.print.Book;
+import java.io.BufferedReader; 
+import java.io.IOException; 
+import java.nio.charset.StandardCharsets; 
+import java.nio.file.Files; 
+import java.nio.file.Path; 
+import java.nio.file.Paths; 
+import java.util.ArrayList; 
+import java.util.List;
+
+public class CSVReader {
+	public static void main(String... args) 
+	{ 
+		List<Book> books = readBooksFromCSV("books.txt"); 
+		
+		for(Book b : books) 
+		{
+			System.out.println(b);
+		}
+	}
+	
+	private static List<Book> readBooksFromCSV(String fileName)
+	{
+		List<Book> books = new ArrayList<Book>();
+		Path pathToFile = Paths.get(fileName);
+		
+		try(BufferedReader br = Files.newBufferedReader(pathToFile, 
+				StandardCharsets.US_ASCII))
+		{
+			String line = br.readLine();
+			
+			while(line != null)
+			{
+				String[] attributes = line.split(",");
+				
+				Book book = createBook(attributes);
+				books.add(book);
+				
+				line = br.readLine();
+			}
+		}
+		catch(IOException ioe)
+		{
+			ioe.printStackTrace();
+		}
+		
+		return books;
+	}
+	
+	private static Book createBook(String[] metadata)
+	{
+		String name = metadata[0];
+		int price = Integer.parseInt(metadata[1]);
+		String author = metadata[2];
+	}
+	
+}
